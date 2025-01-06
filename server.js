98bd2de8ba15c607c8eb9d4773a2be10540afb59
taimoor-ai <taimooor786786@gmail.com>
2025-01-06 22:17:04 +0500

    7

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 app.get('/myApi', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, World from my Api!');
 });
 
 app.listen(PORT, () => {
