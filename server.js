ee95be675bfad0b7a345bdbf00de259342a06c43
taimoor-ai <taimooor786786@gmail.com>
2025-01-06 22:14:35 +0500

    5

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000; // Default to 3000 for local development
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+app.get('/myApi', (req, res) => {
+    res.send('Hello, World!');
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
